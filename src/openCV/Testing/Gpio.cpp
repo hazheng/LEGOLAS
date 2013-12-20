b1fa83b89d67055ba6178e5bddc6f86b3d7980c9
unknownterror <maxyu.2006@gmail.com>
2013-12-20 22:37:24 +0000

    gpioTest is executable for turning pin on pi on and off minor modifications to Gpio.cpp for testing

--- src/openCV/Testing/Gpio.cpp
+++ src/openCV/Testing/Gpio.cpp
@@ -148,21 +148,8 @@ int main(int argc, char *argv[])
 	if (-1 == GPIODirection(POUT, OUT) || -1 == GPIODirection(PIN, IN))
 		return(2);
 
-	do {
-		/*
-		 * Write GPIO value
-		 */
-		if (-1 == GPIOWrite(POUT, repeat % 2))
-			return(3);
-
-		/*
-		 * Read GPIO value
-		 */
-		printf("I'm reading %d in GPIO %d\n", GPIORead(PIN), PIN);
-
-		usleep(500 * 1000);
-	}
-	while (repeat--);
+	GPIOWrite(POUT, HIGH);
+	GPIOWrite(POUT, LOW);
 
 	/*
 	 * Disable GPIO pins
