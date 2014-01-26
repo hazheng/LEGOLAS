bde2a523e732ef491a6dbd4424ca994cdcc2a5e2
hazheng <hazheng@mbhs.edu>
2014-01-25 22:51:05 -0500

    added hsv conversion and display

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/opencv.hpp"
 #include <stdio.h>
 
-using namespace std;
-
-cv::Mat threshIm(cv::Mat image_in){
+/**
+is supposed to be thresholding code
+*/
+cv::Mat threshIm(cv::Mat* image_in){
 	cv::Mat hsv;
-	cv::cvtColor(image_in,hsv,cv::CV_BGR2HSV);
+	cv::cvtColor(*image_in,hsv,CV_BGR2HSV);
 	return hsv;
 }
 
@@ -17,17 +19,23 @@ int main(int argc, char** argv)
 		printf("%s\n","Usage: ./binary imagePath" );
 		return -1;
 	}
-	cv::Mat image;
-	image = cv::imread(argv[1], cv::IMREAD_COLOR); // reads file in path provided
+	cv::Mat src;
+	cv::Mat proc;
+	src = cv::imread(argv[1], cv::IMREAD_COLOR); // reads file in path provided
 
-	if (!image.data)
+	if (!src.data)
 	{
 		printf("%s\n","image not found" );
 		return -1;
 	}
 	//image proc section
-	cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE ); // Create a window for display.
-    cv::imshow( "Display window", image );                // Show our image inside it.
+	proc = threshIm(&src);
+
+	cv::namedWindow( "Source_Image", cv::WINDOW_AUTOSIZE ); // Create a window for display.
+    cv::imshow( "Source_Image", src );                // Show our image inside it.
+
+    cv::namedWindow( "Processed_Image", cv::WINDOW_AUTOSIZE ); // Create a window for processed image.
+    cv::imshow( "Processed_Image", proc );                
 
     cv::waitKey(0); // Wait for a keystroke in the window
 	return 0;
