aa1a25be6ffaa5bf5097e70d96b2c4be4135eb6f
hazheng <hazheng@mbhs.edu>
2014-01-25 22:13:50 -0500

    added makefile and basic image display template

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	if (argc!=2)
+	{
+		printf("%s\n","Usage: ./binary imagePath" );
+		return -1;
+	}
+	cv::Mat image;
+	image = cv::imread(argv[1], cv::IMREAD_COLOR); // reads file in path provided
+
+	if (!image.data)
+	{
+		printf("%s\n","image not found" );
+		return -1;
+	}
+	//image proc section
+	cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE ); // Create a window for display.
+    cv::imshow( "Display window", image );                // Show our image inside it.
+
+    cv::waitKey(0); // Wait for a keystroke in the window
+	return 0;
+}
