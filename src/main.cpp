83f26462dc83fbd0f34a78c40a3a7448fdbc8fb0
hazheng <hazheng@mbhs.edu>
2014-01-27 16:55:56 -0500

    commented up the code and changed all chars to uint8_t also added debug flags to the makefile

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,13 @@
 #include <stdint.h>
 
 using namespace cv;
-
+/**
+checks pixel represented by a rgb array (src) and determined if it's a "hot" pixel or not
+note that the vals are 8 bit unsigned numerics
+*/
 bool check_hit(char src[]){
-	uint8_t l_cut[3] = {0,100,0};
-	uint8_t u_cut[3] = {150,230,150};
+	uint8_t l_cut[3] = {0,100,0}; //lower cutoff
+	uint8_t u_cut[3] = {150,230,150};//
 	uint8_t temp;
 	for (int i = 0; i < 3; i++)
 	{
@@ -20,16 +23,16 @@ bool check_hit(char src[]){
 	return true;
 }
 /**
-is supposed to be thresholding code
+takes a image and returns a b and w image
+target areas are 255(white), non target are 0 (black) 
 */
 Mat threshIm(Mat image_in){
 	Mat channels[3];
 	Mat mask(image_in.size(),CV_8UC1);
 	mask = Mat::zeros(mask.rows,mask.cols,CV_8U);
 	split(image_in,channels);
-	unsigned char hit = 255; // remeber has got to be a 8bit datatype
-	char temp[3];
-	char derp;
+	uint8_t hit = 255; // remeber has got to be a 8bit datatype
+	uint8_t temp[3];
 
 	for (int i = 0; i < (mask.rows); i++)
 	{
@@ -37,9 +40,7 @@ Mat threshIm(Mat image_in){
 			{	
 					for (int k = 0; k < 3; k++)
 					{
-						derp = channels[k].at<char>(i,j);
-						// printf("%d ",derp );
-						temp[k]=derp;
+						temp[k]=channels[k].at<char>(i,j);
 					}
 					if(check_hit(temp))
 						mask.at<char>(i,j) = hit; //make sure to actually use the correct ref (<char>) otherwise shit overflows
@@ -49,6 +50,10 @@ Mat threshIm(Mat image_in){
 	return mask;
 }
 
+/**
+main method
+at present it loads a source image, processes the image then displays both
+*/
 int main(int argc, char** argv)
 { 
 	if (argc!=2)
